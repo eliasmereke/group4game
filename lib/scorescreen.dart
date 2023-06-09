af0208ed2c6a6c6c0f1682306a254b3a426732b3
Yomi <121083266+Yomidan1@users.noreply.github.com>
2023-06-09 00:38:47 -0700

    scorescreen.dart

--- lib/scorescreen.dart
+++ lib/scorescreen.dart
@@ -1,43 +1,40 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
-class MyBall extends StatelessWidget {
-  final x;
-  final y;
+class ScoreScreen extends StatelessWidget {
   final bool gameHasStarted;
-
-  MyBall({this.x, this.y, required this.gameHasStarted});
-
+  final enemyScore;
+  final playerScore;
+  ScoreScreen(
+      {required this.gameHasStarted, this.enemyScore, this.playerScore});
   @override
   Widget build(BuildContext context) {
     return gameHasStarted
-        ? Container(
-            alignment: Alignment(x, y),
-            child: Container(
-              decoration:
-                  BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-              width: 20,
-              height: 20,
-            ),
-          )
-        : Container(
-            alignment: Alignment(x, y),
-            child: AvatarGlow(
-              endRadius: 60.0,
-              child: Material(
-                elevation: 8.0,
-                shape: CircleBorder(),
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey[100],
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.white),
-                    width: 30,
-                    height: 30,
-                  ),
-                  radius: 7.0,
+        ? Stack(
+            children: [
+              Container(
+                alignment: Alignment(0, 0),
+                child: Container(
+                  height: 1,
+                  width: MediaQuery.of(context).size.width / 4,
+                  color: Colors.grey[700],
                 ),
               ),
-            ));
+              Container(
+                alignment: Alignment(0, -0.3),
+                child: Text(
+                  enemyScore.toString(),
+                  style: TextStyle(color: Colors.grey[800], fontSize: 100),
+                ),
+              ),
+              Container(
+                alignment: Alignment(0, 0.3),
+                child: Text(
+                  playerScore.toString(),
+                  style: TextStyle(color: Colors.grey[800], fontSize: 100),
+                ),
+              ),
+            ],
+          )
+        : Container();
   }
 }
